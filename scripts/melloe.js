affa714e02ec63744fd6ae20cac06a3e7d490d47
nzhul <dobromirivanov1@gmail.com>
2016-12-15 21:42:38 +0200

    Improving responsive design for index.html

--- scripts/melloe.js
+++ scripts/melloe.js
@@ -1,25 +1,28 @@
 $(document).ready(function () {
-	var products = $('.full-product-wrapper');
-	var zoomIcon = $('<i class="fa fa-search zoom-hover-icon"></i>');
 
-	products.on('mouseenter', function () {
-		var theProduct = $(this);
-		theProduct.prepend(zoomIcon[0]);
-		zoomIcon.on('click', function () {
-			$(this).parent().find('a.thumbnail-link')[0].click();
+	if(window.screen.width > 991){
+		var products = $('.full-product-wrapper');
+		var zoomIcon = $('<i class="fa fa-search zoom-hover-icon"></i>');
+
+		products.on('mouseenter', function () {
+			var theProduct = $(this);
+			theProduct.prepend(zoomIcon[0]);
+			zoomIcon.on('click', function () {
+				$(this).parent().find('a.thumbnail-link')[0].click();
+			});
+			zoomIcon.animate({
+				opacity: 0.55,
+				top: "+=20",
+			}, 300, function () {
+				// Animation complete.
+			});
 		});
-		zoomIcon.animate({
-			opacity: 0.55,
-			top: "+=20",
-		}, 300, function () {
-			// Animation complete.
+		products.on('mouseleave', function () {
+			var theProduct = $(this);
+			var foundIcon = theProduct.find('i.fa-search');
+			foundIcon.offset({ top: theProduct.offset().top + theProduct.width() / 2.9, left: theProduct.offset().left + theProduct.height() / 3.15 });
+			foundIcon.css('opacity', '0');
+			foundIcon.remove();
 		});
-	});
-	products.on('mouseleave', function () {
-		var theProduct = $(this);
-		var foundIcon = theProduct.find('i.fa-search');
-		foundIcon.offset({ top: theProduct.offset().top + theProduct.width() / 2.9, left: theProduct.offset().left + theProduct.height() / 3.15 });
-		foundIcon.css('opacity', '0');
-		foundIcon.remove();
-	});
+	}
 });
